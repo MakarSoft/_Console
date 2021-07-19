e9ffb2b62a927a463d25f70e7e4bce129e6b2ae0
Alex Mkr <alex.mkr@gmail.com>
2021-07-19 16:14:50 +0300

    Простой класс "Бабочка"

--- Console_01/Program.cs
+++ Console_01/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
+
 namespace Console_01
 {
+    class Baterfly
+    {
+        
+        public string name;
+        private int _maxCount;
+        public Baterfly()
+        {
+            _maxCount = 100;
+        }
+        public Baterfly(int maxCount)
+        {
+            _maxCount = maxCount;
+        }
+        public void Fly(int count)
+        {
+            if (count > 0) {
+                if (count >= _maxCount)
+                    count = _maxCount;
+                for (int i = 0; i < count; i++)
+                    Console.WriteLine("Бяг");
+            }
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Первое консольное приложение");
+            Baterfly btf = new Baterfly(200);
+            btf.Fly(3);
+            Console.ReadKey();
         }
     }
 }
